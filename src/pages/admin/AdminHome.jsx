976f4246e8e33d800902f7cf4832edb36aed10cf
Nairo Elsner <nairo.elsner@hotmail.com>
2023-11-30 04:03:40 -0300

    update admin home

--- src/pages/admin/AdminHome.jsx
+++ src/pages/admin/AdminHome.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-//import ResumeComponent from "../../components/admin/ResumeComponent";
+import ResumeComponent from "../../components/admin/ResumeComponent";
 import SellersTableComponent from "../../components/admin/SellersTableComponent";
 import OrdersTableComponent from "../../components/admin/OrdersTableComponent";
 
 const AdminHome = () => {
     return(
         <>
-            
+            <ResumeComponent/>
             <div className='tables'>
               <SellersTableComponent/>
               <OrdersTableComponent/>
