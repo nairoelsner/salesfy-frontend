411f159f56367a9e41cd23ef531b9b0fb6c7b28f
Nairo Elsner <nairo.elsner@hotmail.com>
2023-11-30 03:23:47 -0300

    update path

--- src/pages/admin/AdminHome.jsx
+++ src/pages/admin/AdminHome.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ResumeComponent from "../../components/admin/ResumeComponent";
+import ResumeComponent from "../../components/admin/resumecomponent";
 import SellersTableComponent from "../../components/admin/SellersTableComponent";
 import OrdersTableComponent from "../../components/admin/OrdersTableComponent";
 
